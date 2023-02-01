ce4c6825ef7c1be742d637d13c642453d3e852be
David Nahodyl <david@gearboxgo.com>
2023-02-01 14:23:21 -0500

    post-shift 5.5 changes

---
+++ resources/views/errors/error.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="container">
+        <div class="row">
+            <h2>Error!</h2>
+        	<p>Oops! We ran into an error. Please try again.</p>
+            <p>Please don't hesitate to email us at <a href="mailto:info@skihire2u.com" target="_blank">info@skihire2u.com</a> or call us on <a href="tel:+33450721970" value="+33450721970" target="_blank">+33 4 50 72 19 70</a></p>
+
+            <a href="/" class="btn btn-success">Start Over</a>
+        </div>
+    </div>
+@endsection
