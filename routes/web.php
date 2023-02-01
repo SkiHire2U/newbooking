b1e48b1e2dd1c1e996f690f72903e2bb89d84e4b
Shift <shift@laravelshift.com>
2023-02-01 20:19:42 +0000

    Adopt class based routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\ErrorController;
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,61 +19,61 @@
 
 Auth::routes();
 
-Route::get('/', 'PageController@getIndex');
-Route::get('/home', 'PageController@redirectIndex');
-Route::post('/dateDetails', 'PageController@postDateDetails')->name('dateDetails');
-Route::get('/equipments', 'PageController@getEquipments')->name('equipments');
-Route::post('/addToRack', 'PageController@postAddToRack')->name('addToRack');
-Route::post('/removeFromRack/{name}', 'PageController@postRemoveFromRack')->name('removeFromRack');
-Route::get('/rentals', 'PageController@getRentals')->name('rentals');
-Route::post('/saveRenter/{name}', 'PageController@postSaveRenter')->name('saveRenter');
-Route::post('/editRenter/{name}', 'PageController@postEditRenter')->name('editRenter');
-Route::post('/removeFromRental/{name}', 'PageController@postRemoveFromRental')->name('removeFromRental');
-Route::get('/details', 'PageController@getPartyDetails')->name('details');
+Route::get('/', [PageController::class, 'getIndex']);
+Route::get('/home', [PageController::class, 'redirectIndex']);
+Route::post('/dateDetails', [PageController::class, 'postDateDetails'])->name('dateDetails');
+Route::get('/equipments', [PageController::class, 'getEquipments'])->name('equipments');
+Route::post('/addToRack', [PageController::class, 'postAddToRack'])->name('addToRack');
+Route::post('/removeFromRack/{name}', [PageController::class, 'postRemoveFromRack'])->name('removeFromRack');
+Route::get('/rentals', [PageController::class, 'getRentals'])->name('rentals');
+Route::post('/saveRenter/{name}', [PageController::class, 'postSaveRenter'])->name('saveRenter');
+Route::post('/editRenter/{name}', [PageController::class, 'postEditRenter'])->name('editRenter');
+Route::post('/removeFromRental/{name}', [PageController::class, 'postRemoveFromRental'])->name('removeFromRental');
+Route::get('/details', [PageController::class, 'getPartyDetails'])->name('details');
 
 /* REFERENCE */
-Route::post('/reference-details/{id}', 'PageController@postUpdateDateDetails')->name('reference.updateDetails');
-Route::post('/reference/', 'PageController@postReference')->name('reference');
-Route::post('/reference/{id}', 'BookingController@updateBooking')->name('reference.update');
-Route::get('/updated/', 'BookingController@updated')->name('updated');
+Route::post('/reference-details/{id}', [PageController::class, 'postUpdateDateDetails'])->name('reference.updateDetails');
+Route::post('/reference/', [PageController::class, 'postReference'])->name('reference');
+Route::post('/reference/{id}', [BookingController::class, 'updateBooking'])->name('reference.update');
+Route::get('/updated/', [BookingController::class, 'updated'])->name('updated');
 
-Route::get('/admin/packages', 'AdminController@getPackages')->name('packages');
-Route::put('/admin/package', 'AdminController@storePackage')->name('package.store');
-Route::put('/admin/package/{id}', 'AdminController@updatePackage')->name('package.update');
-Route::post('/admin/package/{id}', 'AdminController@deletePackage')->name('package.delete');
-Route::get('/admin/accommodations', 'AdminController@getAccommodations')->name('accommodations');
-Route::get('/admin/accommodations/{id}', 'AdminController@showAccommodation')->name('accommodation');
-Route::put('/admin/accommodations/', 'AdminController@storeAccommodation')->name('accommodation.store');
-Route::put('/admin/accommodations/{id}', 'AdminController@updateAccommodation')->name('accommodation.update');
-Route::post('/admin/accommodations/{id}', 'AdminController@deleteAccommodation')->name('accommodation.delete');
-Route::put('/admin/operators/', 'AdminController@storeOperator')->name('operator.store');
-Route::put('/admin/operators/{id}', 'AdminController@updateOperator')->name('operator.update');
-Route::post('/admin/operators/{id}', 'AdminController@deleteOperator')->name('operator.delete');
-Route::get('/admin/bookings', 'AdminController@getBookings')->name('bookings');
-Route::post('/admin/bookings-filter', 'AdminController@filterBookings')->name('bookings.filter');
-Route::get('/admin/bookings/{id}', 'AdminController@showBooking')->name('booking');
-Route::post('/admin/booking-reference', 'AdminController@findBookingReference')->name('booking.reference');
-Route::put('/admin/update-booking/{id}', 'AdminController@updateBooking')->name('bookings.update');
-Route::post('/admin/delete-booking/{id}', 'AdminController@deleteBooking')->name('booking.delete');
-Route::post('/admin/booking/{id}', 'AdminController@notifyCustomer')->name('booking.email');
-Route::put('/admin/rental/{id}', 'AdminController@updateRental')->name('rental.update');
-Route::post('/admin/rental/{id}', 'AdminController@postRemoveFromList')->name('rental.delete');
-Route::get('/admin/picking-list/{id}', 'AdminController@getPickingList')->name('picking');
-Route::get('/admin/invoice/{id}', 'AdminController@getInvoice')->name('invoice');
-Route::get('/admin/invoiceEdit/{id}', 'AdminController@editInvoice')->name('invoice.edit');
-Route::put('/admin/invoiceUpdate/{id}', 'AdminController@updateInvoice')->name('invoice.update');
-Route::get('/admin', 'AdminController@index');
+Route::get('/admin/packages', [AdminController::class, 'getPackages'])->name('packages');
+Route::put('/admin/package', [AdminController::class, 'storePackage'])->name('package.store');
+Route::put('/admin/package/{id}', [AdminController::class, 'updatePackage'])->name('package.update');
+Route::post('/admin/package/{id}', [AdminController::class, 'deletePackage'])->name('package.delete');
+Route::get('/admin/accommodations', [AdminController::class, 'getAccommodations'])->name('accommodations');
+Route::get('/admin/accommodations/{id}', [AdminController::class, 'showAccommodation'])->name('accommodation');
+Route::put('/admin/accommodations/', [AdminController::class, 'storeAccommodation'])->name('accommodation.store');
+Route::put('/admin/accommodations/{id}', [AdminController::class, 'updateAccommodation'])->name('accommodation.update');
+Route::post('/admin/accommodations/{id}', [AdminController::class, 'deleteAccommodation'])->name('accommodation.delete');
+Route::put('/admin/operators/', [AdminController::class, 'storeOperator'])->name('operator.store');
+Route::put('/admin/operators/{id}', [AdminController::class, 'updateOperator'])->name('operator.update');
+Route::post('/admin/operators/{id}', [AdminController::class, 'deleteOperator'])->name('operator.delete');
+Route::get('/admin/bookings', [AdminController::class, 'getBookings'])->name('bookings');
+Route::post('/admin/bookings-filter', [AdminController::class, 'filterBookings'])->name('bookings.filter');
+Route::get('/admin/bookings/{id}', [AdminController::class, 'showBooking'])->name('booking');
+Route::post('/admin/booking-reference', [AdminController::class, 'findBookingReference'])->name('booking.reference');
+Route::put('/admin/update-booking/{id}', [AdminController::class, 'updateBooking'])->name('bookings.update');
+Route::post('/admin/delete-booking/{id}', [AdminController::class, 'deleteBooking'])->name('booking.delete');
+Route::post('/admin/booking/{id}', [AdminController::class, 'notifyCustomer'])->name('booking.email');
+Route::put('/admin/rental/{id}', [AdminController::class, 'updateRental'])->name('rental.update');
+Route::post('/admin/rental/{id}', [AdminController::class, 'postRemoveFromList'])->name('rental.delete');
+Route::get('/admin/picking-list/{id}', [AdminController::class, 'getPickingList'])->name('picking');
+Route::get('/admin/invoice/{id}', [AdminController::class, 'getInvoice'])->name('invoice');
+Route::get('/admin/invoiceEdit/{id}', [AdminController::class, 'editInvoice'])->name('invoice.edit');
+Route::put('/admin/invoiceUpdate/{id}', [AdminController::class, 'updateInvoice'])->name('invoice.update');
+Route::get('/admin', [AdminController::class, 'index']);
 
 /* BOOKING */
-Route::get('/booking/showAccommodationSelection.action', 'PageController@redirectIndex');
-Route::resource('booking', 'BookingController');
+Route::get('/booking/showAccommodationSelection.action', [PageController::class, 'redirectIndex']);
+Route::resource('booking', BookingController::class);
 
 /* IMPORT */
-Route::get('/import', 'BookingController@runImport')->name('import');
-Route::get('/import-alt', 'BookingController@runImportAlt');
+Route::get('/import', [BookingController::class, 'runImport'])->name('import');
+Route::get('/import-alt', [BookingController::class, 'runImportAlt']);
 
 /* ERROR */
-Route::get('/session-expired/', 'ErrorController@getSessionExpired')->name('error.expired');
+Route::get('/session-expired/', [ErrorController::class, 'getSessionExpired'])->name('error.expired');
 
 /* SUPERADMIN */
-Route::get('/generate-invoices/', 'AdminController@generateInvoicesAll');
+Route::get('/generate-invoices/', [AdminController::class, 'generateInvoicesAll']);
