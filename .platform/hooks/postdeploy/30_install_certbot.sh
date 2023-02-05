0bb9f4a4f30c4e976537731c04d36e9da5c7670c
David Nahodyl <david@gearboxgo.com>
2023-02-04 19:17:38 -0500

    added check to see if certbot is already installed

--- .platform/hooks/postdeploy/30_install_certbot.sh
+++ .platform/hooks/postdeploy/30_install_certbot.sh
@@ -5,9 +5,17 @@ contact="contact@gearboxgo.com"
 
 # this must be done in postdeploy so that nginx config doesn't get overwritten by Elastic Beanstalk
 
+#check if certbot is already installed
+if command -v certbot &> /dev/null
+then
+    echo "certbot already installed"
+    exit
+fi
+
 #instructions from https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/SSL-on-amazon-linux-2.html#letsencrypt
 
 #install prerequisites
+cd /tmp
 sudo wget -r --no-parent -A 'epel-release-*.rpm' https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
 sudo rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
 sudo yum-config-manager --enable epel*
