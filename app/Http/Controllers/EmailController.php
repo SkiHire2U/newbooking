a2ea88a0e2bea5e8fa1efe80a887817b88c438a4
David Nahodyl <david@gearboxgo.com>
2023-02-08 19:36:23 -0500

    updated email config to work with Laravel 9

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,31 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Mail;
+use Illuminate\Mail\Mailable;
+use Illuminate\Support\Facades\Mail;
 
 class EmailController extends Controller
 {
     public function sendMail($data)
     {
-        Mail::queue('emails.'.$data['type'], $data, function ($message) use ($data) {
-            $message->from('info@skihire2u.com', 'SkiHire2U');
-            $message->to($data['email']);
-            // $message->to('paul.sepe@webee.com.mt');
-            // $message->bcc('paul.sepe@webee.com.mt');
-            $message->subject($data['subject']);
-        });
+        Mail::queue((new Mailable())
+            ->from('info@skihire2u.com', 'SkiHire2U')
+            ->to($data['email'])
+            ->subject($data['subject'])
+            ->view('emails.' . $data['type'], $data));
     }
+
 
     public function sendAdminMail($data)
     {
-        Mail::queue('emails.'.$data['type'], $data, function ($message) use ($data) {
-            $message->from('info@skihire2u.com', 'SkiHire2U Booking System');
-            $message->to('info@skihire2u.com');
-            // $message->to('paul.sepe@webee.com.mt');
-            // $message->bcc('paul.sepe@webee.com.mt');
-            // $message->bcc('noreply.newskihire2u@gmail.com');
-            $message->subject($data['subject']);
-        });
+        Mail::queue((new Mailable())
+            ->from('info@skihire2u.com', 'SkiHire2U Booking System')
+            ->to('info@skihire2u.com')
+            ->subject($data['subject'])
+            ->view('emails.' . $data['type'], $data));
     }
 
     public function sendErrorMail($data)
