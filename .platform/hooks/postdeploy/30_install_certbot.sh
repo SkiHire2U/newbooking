16e88192c53364f07efa1737ac853b9d0149a541
David Nahodyl <david@gearboxgo.com>
2023-04-07 18:26:22 -0400

    updated certbot deployment script

--- .platform/hooks/postdeploy/30_install_certbot.sh
+++ .platform/hooks/postdeploy/30_install_certbot.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
 
+# ---- Configuration ----
 domain="nb-test.skihire2u.com"
 contact="contact@gearboxgo.com"
+test_mode=true
+# -----------------------
+
+# Temporary immediate exit so that we don't run this until dns is set up
+#exit
 
 # this must be done in postdeploy so that nginx config doesn't get overwritten by Elastic Beanstalk
 
 #check if certbot is already installed
-if command -v certbot &> /dev/null
-then
-    echo "certbot already installed - reinstalling existing certificate"
-    certbot -n -d ${domain} --nginx --reinstall --redirect
-    exit
+if command -v certbot &>/dev/null; then
+    echo "certbot already installed"
+
+    # check if the certificate is staging or production
+    # look for the word "STAGING" in the certificate info
+    # bash returns 0 if it finds a match
+    openssl x509 -in /etc/letsencrypt/live/${domain}/cert.pem -text -noout | grep -q STAGING
+    result=$?
+    if [ "$result" = 0 ]; then
+        is_staging=true
+    else
+        is_staging=false
+    fi
+
+    if [ "$test_mode" = false ] && [ "$is_staging" = true ]; then
+        # Current certificate is from staging but we need production
+        # Force install a production certificate
+        echo "Staging SSL certificate is installed, but we need production"
+        echo "Forcing SSL certificate renewal..."
+
+        sudo certbot -n -d ${domain} --nginx --agree-tos --email ${contact} --redirect --force-renewal
+        exit
+    else
+        #just reinstall
+        echo "Reinstalling existing SSL certificate from certbot"
+        certbot -n -d ${domain} --nginx --reinstall --redirect --agree-tos --email ${contact}
+        exit
+    fi
+
 fi
 
-#instructions from https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/SSL-on-amazon-linux-2.html#letsencrypt
+# Install certbot since it's not installed already
+# Instructions from https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/SSL-on-amazon-linux-2.html#letsencrypt
 
 #install prerequisites
 cd /tmp
@@ -26,9 +57,13 @@ sudo amazon-linux-extras install epel -y
 sudo yum install -y certbot python2-certbot-nginx
 
 # get certificate
-# test mode
-#sudo certbot -n -d ${domain} --nginx --agree-tos --email ${contact} --redirect --test-cert
-sudo certbot -n -d ${domain} --nginx --agree-tos --email ${contact} --redirect
+if [ "$test_mode" = true ]; then
+    #get a test mode cert
+    sudo certbot -n -d ${domain} --nginx --agree-tos --email ${contact} --redirect --test-cert
+else
+    #get a production cert
+    sudo certbot -n -d ${domain} --nginx --agree-tos --email ${contact} --redirect
+fi
 
 #add cron job
 touch /etc/cron.d/certbot_renew
